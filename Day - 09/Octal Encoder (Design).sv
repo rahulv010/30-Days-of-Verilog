8298346f5e270da48fda1f2b6ca7b335648ea590
rahulv010 <cvrahuloff@gmail.com>
2024-07-07 00:35:41 +0530

    Create Octal Encoder (Design).sv

---
+++ Day - 09/Octal Encoder (Design).sv
@@ -0,0 +1,8 @@
+//Encoder 8:3
+module encoder(a,b);
+  input [7:0] a;
+  output [2:0] b;
+  assign b[0] = a[7]|a[5]|a[3]|a[1];
+  assign b[1] = a[7]|a[6]|a[3]|a[2];
+  assign b[2] = a[7]|a[6]|a[5]|a[4];
+endmodule
