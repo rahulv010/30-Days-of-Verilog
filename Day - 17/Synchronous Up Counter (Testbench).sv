391825187e0c56a34bb6c1845db04600c908d676
rahulv010 <cvrahuloff@gmail.com>
2024-07-07 00:43:30 +0530

    Create Synchronous Up Counter (Testbench).sv

---
+++ Day - 17/Synchronous Up Counter (Testbench).sv
@@ -0,0 +1,19 @@
+//Synchronous Up Counter
+module counter_tb;
+  reg t1,clk1;
+  wire [3:0] q1,qb1;
+  counter dut (.t(t1),.clk(clk1),.q(q1),.qb(qb1));
+  initial begin
+    clk1= 1'b0;
+    forever #2 clk1 = ~clk1;
+  end
+  initial begin
+    $dumpfile("Counters.vcd");
+    $dumpvars (1);
+    $monitor ("Time: %0d, Output: %b", $time, q1);
+    t1 = 1'b1;
+    #66;
+    $finish;
+  end
+endmodule
+
